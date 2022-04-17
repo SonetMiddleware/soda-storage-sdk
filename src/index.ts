eaf656be927c457479cbe644eed6134576b46af8
MickeyWang <1244134672@qq.com>
2022-04-18 00:14:48 +0800

    first commit

---
+++ src/index.ts
@@ -0,0 +1,35 @@
+const _storageServices = {}
+
+const registerStorageService = (service: any) => {
+  _storageServices[service.name] = service.meta
+}
+
+const getStorageService = (serviceNames?: string[]) => {
+  if (!serviceNames || serviceNames.length === 0) {
+    return _storageServices
+  }
+  const ret = {}
+  for (const item of serviceNames) {
+    ret[item] = _storageServices[item] || null
+  }
+  return ret
+}
+
+const store = async (service: string, source: string, paymentMeta?: any) => {
+  if (!_storageServices[service]) {
+    return null
+  }
+  const res = await _storageServices[service].storeFunc(source)
+  return res
+}
+
+const load = async (service: string, uri: string) => {
+  if (!_storageServices[service]) {
+    return null
+  }
+  const source = await _storageServices[service].loadFunc(uri)
+  return source
+}
+
+export { store, load, getStorageService, registerStorageService }
+export default _storageServices
